eba8188f0b839ff3297b7048d4d5af7ff51f0aad
Franz Manguray <franznmanguray@gmail.com>
2024-05-27 10:41:16 -0700

    gunicorn requirements

--- tests/conftest.py
+++ tests/conftest.py
@@ -74,6 +74,7 @@ def init_database(test_client):
         user_id=test_user1.id,
         sentiment='POSITIVE',
     )
+    
     test_post2 = Post(
         message='mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero justo laoreet sit amet cursus sit amet dictum sit amet justo',
         user_id=test_user2.id,
